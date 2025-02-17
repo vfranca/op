617ef5d56c92ef6fb5c395b2399f03a8083bdebe
Valmir França <vfranca3@gmail.com>
2025-02-17 17:03:31 -0300

    Alterada nome da opcao trades para operacoes

--- em.py
+++ em.py
@@ -9,12 +9,12 @@ import click
 @click.argument("taxa-acerto", type=int)
 @click.argument("lucro", type=float)
 @click.argument("prejuizo", type=float)
-@click.option("--trades", "-t", type=int, default=10)
-def em(taxa_acerto, lucro, prejuizo, trades):
+@click.option("--operacoes", "-o", type=int, default=10)
+def em(taxa_acerto, lucro, prejuizo, operacoes):
     """Calcula expectativa matematica do trade system"""
     lucro_medio = (taxa_acerto / 100 * lucro) - ((100 - taxa_acerto) / 100 * prejuizo)
-    lucro_total = lucro_medio * trades
-    click.echo("operacoes %i" % trades)
+    lucro_total = lucro_medio * operacoes
+    click.echo("operacoes %i" % operacoes)
     click.echo("lucro medio %.2f" % lucro_medio)
     click.echo("lucro total %.2f" % lucro_total)
 
