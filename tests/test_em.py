4918c025c2242fec73ccc1f8bb64a4eee19657af
Valmir França <vfranca3@gmail.com>
2025-02-17 17:08:35 -0300

    Adicionado mais um teste para em

--- tests/test_em.py
+++ tests/test_em.py
@@ -8,3 +8,8 @@ run = CliRunner()
 def test_calcula_expectativa_matematica():
     res = run.invoke(op, ["em", "60", "300", "150"])
     assert res.output == "operacoes 10\nlucro medio 120.00\nlucro total 1200.00\n"
+
+
+def test_calcula_expectativa_matematica_com_opcao_operacoes():
+    res = run.invoke(op, ["em", "--operacoes", "40", "60", "300", "150"])
+    assert res.output == "operacoes 40\nlucro medio 120.00\nlucro total 4800.00\n"
