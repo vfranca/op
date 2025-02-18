21ef55b3b32aeb5f6575936ae30ee1a822b1da6a
Valmir França <vfranca3@gmail.com>
2025-02-17 22:50:44 -0300

    Adicionado teste para cfd

--- tests/test_cfd.py
+++ tests/test_cfd.py
@@ -4,6 +4,11 @@ from op import op
 run = CliRunner()
 
 
+def test_calcula_cfd_comvariaveis_de_ambiente():
+    res = run.invoke(op, ["cfd"])
+    assert res.output == "4000000.00\n"
+
+
 def test_calcula_valor_do_cfd_com_opcao_tamanho():
     res = run.invoke(op, ["cfd", "40", "0.1", "--tamanho", "1"])
     assert res.output == "4.00\n"
