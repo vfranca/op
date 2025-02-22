d23a2f73f174d9e0551e7b7ae0328e1815852fbf
Valmir França <vfranca3@gmail.com>
2025-02-22 12:36:41 -0300

    Adicionado teste de venda

--- op/e.py
+++ op/e.py
@@ -10,18 +10,21 @@ import click
 @click.option("--risco", "-r", type=float)
 @click.option("--retorno", "-rr", type=int)
 @click.option("--digitos", "-d", type=int, default=0)
-def e(entrada, risco, retorno, digitos):
+@click.option("--tipo", "-t", default="c")
+def e(entrada, risco, retorno, digitos, tipo):
     """Calcula operacao de trading"""
-    sl = entrada - risco
+    if tipo == "v":
+        entrada = -entrada  # somente para operação de venda
+    sl = entrada - risco  # stop loss
     targets = []
     for rr in range(retorno):
         targets.append(entrada + risco * (rr + 1))
     i = len(targets)
     for tg in reversed(targets):
-        click.echo("%.{0}f %iX".format(digitos) % (tg, i))
+        click.echo("%.{0}f %iX".format(digitos) % (abs(tg), i))
         i -= 1
-    click.echo("%.{0}f E".format(digitos) % entrada)
-    click.echo("%.{0}f SL".format(digitos) % sl)
+    click.echo("%.{0}f E".format(digitos) % abs(entrada))
+    click.echo("%.{0}f SL".format(digitos) % abs(sl))
 
 
 if __name__ == "__main__":
--- tests/test_e.py
+++ tests/test_e.py
@@ -7,3 +7,8 @@ run = CliRunner()
 def test_calcula_uma_compra():
     res = run.invoke(op, ["e", "130200", "-r", "100", "-rr", "2"])
     assert res.output == "130400 2X\n130300 1X\n130200 E\n130100 SL\n"
+
+
+def test_calcula_uma_venda():
+    res = run.invoke(op, ["e", "130200", "-r", "100", "-rr", "2", "-t", "v"])
+    assert res.output == "130000 2X\n130100 1X\n130200 E\n130300 SL\n"
