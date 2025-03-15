803a00a195e36ebe2fab51a92e873078565dc0d7
Valmir França <vfranca3@gmail.com>
2025-03-14 22:37:37 -0300

    Revisado preco medio pm.py

--- op/pm.py
+++ op/pm.py
@@ -3,18 +3,23 @@ Aplicativo CLI para calcular o preço médio
 """
 
 import click
-from os import getenv
-
-d = getenv("DIGITOS")
 
 
 @click.command()
 @click.argument("e1", type=float, envvar="e1")
-@click.argument("e2", type=float, envvar="e2")
-@click.argument("e3", type=float, envvar="e3")
-@click.argument("e4", type=float, envvar="e4")
-@click.argument("e5", type=float, envvar="E5")
-def pm(e1, e2, e3, e4, e5):
+@click.argument("e2", type=float, envvar="e2", required=False)
+@click.argument("e3", type=float, envvar="e3", required=False)
+@click.argument("e4", type=float, envvar="e4", required=False)
+@click.argument("e5", type=float, envvar="E5", required=False)
+@click.option(
+    "--digitos",
+    "-d",
+    type=int,
+    default=2,
+    envvar="DIGITOS",
+    help="Digito da moeda, default 2.",
+)
+def pm(e1, e2, e3, e4, e5, digitos):
     """Calcula o preco medio"""
     medio = e1
     if e2:
@@ -25,7 +30,7 @@ def pm(e1, e2, e3, e4, e5):
         medio = (e1 + e2 + e3 + e4) / 4
     if e5:
         medio = (e1 + e2 + e3 + e4 + e5) / 5
-    click.echo("%.{0}f".format(d) % medio)
+    click.echo("%.{0}f".format(digitos) % medio)
 
 
 if __name__ == "__main__":
